1079ff96dc32729d31af1d548df789b8c367508a
Nazmul-SWE <nazmulmridha680@gmail.com>
2025-04-26 16:43:03 +0600

    Category to product update)

--- FutureFinds/urls.py
+++ FutureFinds/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', viwes.home),
     path('product/', include('products.urls')),
+    path('products/', viwes.products),
 ]
--- FutureFinds/viwes.py
+++ FutureFinds/viwes.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Category
+
 
 def home (request):
     products = Product.objects.all()
-    return render(request,'home.html',context={'products':products})
+    categories = Category.objects.all()
+    return render(request, 'home.html', {'products': products, 'categories': categories})
+
+def products (request):
+    products = Product.objects.all()
+    return render(request,'products.html',context={'products':products})
+
